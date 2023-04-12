6840a0be7a645e6a24dbd5d64ac965d8aae7106f
GL <kalinichev.peter@yandex.ru>
2023-04-12 14:18:36 +0300

    Extends + include

--- chair_shop/mainapp/views.py
+++ chair_shop/mainapp/views.py
@@ -1,13 +1,48 @@
 from django.shortcuts import render
 
+content = {
+    'main_menu': [
+        {'href': 'main', 'name': 'домой'},
+        {'href': 'products', 'name': 'продукты'},
+        {'href': 'contact', 'name': 'контакты'},
+    ]
+
+}
+
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+
+    own_content = {
+        'title': title,
+        'products': products,
+    }
+
+    content.update(own_content)
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+
+    own_content = {
+        'title': 'продукты',
+        'links_menu': links_menu,
+    }
+
+    content.update(own_content)
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    own_content = {
+        'title': 'контакты',
+    }
+    content.update(own_content)
+    return render(request, 'mainapp/contact.html', content)
