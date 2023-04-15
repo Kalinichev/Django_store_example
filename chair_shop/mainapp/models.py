fc89eafb5c26040b3d35f0a2440acbe669dbe16d
GL <newkp9@gmail.com>
2023-04-15 19:25:38 +0300

    2nd model and the migration

--- chair_shop/mainapp/models.py
+++ chair_shop/mainapp/models.py
@@ -12,4 +12,18 @@ class ProductCategory(models.Model):
         db_table = 'product_category'
 
 
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    image = models.ImageField(upload_to="products_images", blank=True)
+    short_desk = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
+    description = models.TextField(verbose_name='описание продукта', blank=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=11, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        db_table = 'product'
 
